9671f35d8e2748f298b488b32f6726e86b221f4a
antasofa7 <antasofa.dev@gmail.com>
2024-11-10 19:54:41 +0700

    create laravel project, add stisla template, add fortify to auth login and register and create users feature.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    // index
+    public function index(Request $request) 
+    {
+        $users = DB::table('users')
+            ->when($request->input('name'), function ($query, $name) {
+                $query->where('name', 'like', '%' . $name . '%')
+                    ->orWhere('email', 'like', '%' . $name . '%');
+            })
+            ->orderBy('created_at', 'DESC')->paginate(10);
+        // $users = DB::table('users')
+        // ->when($request->input('name'). function ($query, $name, $email) {
+        //     $query->where('name'.'like', '%'.$name.'%')
+        //     ->  orWhere('email'.'like', '%'.$email.'%');
+        // })
+        // ->paginate(10);
+        return view('pages.users.index', ['type_menu' => 'users'], compact('users'));
+    }
+
+    // create
+    public function create() 
+    {
+        return view('pages.users.create');
+    }
+
+    // store
+    public function store(Request $request) 
+    {
+        // validate the request
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'role' => 'required|in:admin,staff,user',
+        ]);
+
+        // store the request
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = $request->role;
+        $user->save();
+        
+        return redirect()->route('users.index')->with('success', 'User create successfully');
+    }
+
+    // show
+    public function show($id) 
+    {
+        return view('pages.users.show');
+    }
+
+    // edit
+    public function edit($id) {
+        $user = User::findOrFail($id);
+        return view('pages.users.edit', compact('user'));
+    }
+
+    // update
+    public function update(Request $request, $id) 
+    {
+        // validate the request
+        $request->validate([
+            'name' => 'required',
+            // 'email' => 'required|email|unique:users' .$id,
+            'email' => 'required',
+            'role' => 'required|in:admin,staff,user',
+        ]);
+
+        // update the request
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->save();
+
+        $password = $request->password;
+        // if password not empty
+        if ($password) {
+            $user->password = Hash::make($password);
+            $user->save();
+        }
+        
+        return redirect()->route('users.index')->with('success', 'User update successfully');
+    }
+
+    // destroy
+    public function destroy($id)
+    {
+        // delete the request
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect()->route('users.index')->with('success', 'User delete successfully');
+    }
+}
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,43 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">POS Resto</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="nav-item dropdown {{ $type_menu === 'home' ? 'active' : '' }}">
+                <a href="{{ route('home') }}"
+                    class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                {{-- <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link"
+                            href="{{ url('dashboard-general-dashboard') }}">General Dashboard</a>
+                    </li>
+                    <li class="{{ Request::is('dashboard-ecommerce-dashboard') ? 'active' : '' }}">
+                        <a class="nav-link"
+                            href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
+                    </li>
+                </ul> --}}
+            </li>
+            <li class="nav-item dropdown {{ $type_menu === 'users' ? 'active' : '' }}">
+                <a href="{{ route('users.index') }}"
+                    class="nav-link"><i class="fas fa-users"></i><span>Users</span></a>
+                {{-- <ul class="dropdown-menu">
+                    <li class='{{ Request::is('dashboard-general-dashboard') ? 'active' : '' }}'>
+                        <a class="nav-link"
+                            href="
+                            {{ route('users.index') }}
+                             ">Index</a>
+                    </li>
+                    <li class="{{ Request::is('dashboard-ecommerce-dashboard') ? 'active' : '' }}">
+                        <a class="nav-link"
+                            href="{{ url('dashboard-ecommerce-dashboard') }}">Ecommerce Dashboard</a>
+                    </li>
+                </ul> --}}
+            </li>
+        </ul>
+    </aside>
+</div>
